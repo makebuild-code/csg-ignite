784a138f76418b283282f92d6ccf1f406bba5566
Elisa Sanchez MakeBuild <elisa@makebuild.studio>
2026-03-11 10:35:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+(async function () {
+  const files = [
+    'https://makebuild-code.github.io/csg-ignite/modal.js',
+    'https://makebuild-code.github.io/csg-ignite/hide_toolkit.js',
+    'https://makebuild-code.github.io/csg-ignite/accordions.js',
+  ];
 
+  for (const url of files) {
+    const res = await fetch(url);
+    const code = await res.text();
+    eval(code);
+  }
+})();
